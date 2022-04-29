2198c8976161eec3cb27bc03ee473e534caada27
Paulo <paulo.caviquioli@hotmail.com>
2022-04-29 00:00:16 -0300

    28.04

---
+++ src/routes/produtos.js
@@ -0,0 +1,13 @@
+const express = require('express');
+var router = express.Router();
+
+var controller = require('../controllers/produtos');
+
+router.post('/', controller.incluir)
+router.get('/', controller.listar)
+router.get('/:codigo', controller.buscar)
+router.put('/:codigo', controller.alterar)
+router.delete('/:codigo', controller.excluir)
+
+
+module.exports = router;
